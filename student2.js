a030e1078614a8cbe1da06466155d3fd8eee20cb
yw7sfn47jj-collab <yw7sfn47jj@privaterelay.appleid.com>
2026-06-03 23:02:49 -0700

    Update student2.js

--- student2.js
+++ student2.js
@@ -1,4 +1,16 @@
-var companyname5 = "Izabela's Book Store";
+var product1 = {
+    name:"HP 4426",
+    id:"4426",
+    desc:"Newest and Best Laptop from HP Computer"};
+var product2 = {
+    name:"Apple 88123 iPad",
+    id:"88123",
+    desc:"Apple Tablet Computer"};
+var product3 = {
+    name:"Dell Dimension 2400",
+    id:"2400X",
+    desc:"A fast 2.4 ghz computer"
+};var companyname5 = "Izabela's Book Store";
 var address3 = "492 Main St. Valencia, CA 91344";
 var phonenumber3 = "(555) 869-1234";
 function getHeader() {
